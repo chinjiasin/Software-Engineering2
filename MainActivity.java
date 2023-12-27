b39692720c9d4f6298a3a273d0a6fd892ee2d5ab
chinjiasin <148529467+chinjiasin@users.noreply.github.com>
2023-12-27 15:47:37 +0800

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.a1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button move;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        move = findViewById(R.id.Move);
+        move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Ads1.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
